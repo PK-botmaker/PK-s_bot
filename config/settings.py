f2f57b00e9a3d32c744d300d348d14b184694b97
PK-botmaker <sdhileep075@gmail.com>
2025-05-23 19:43:46 +0530

    Delete config directory

--- config/settings.py
+++
@@ -1,7 +0,0 @@
-from utils.db_channel import load_json
-
-SETTINGS_FILE = "config/settings.json"
-
-def load_settings():
-    """📂 Load bot settings (placeholder for future config)."""
-    return load_json(SETTINGS_FILE, {})
